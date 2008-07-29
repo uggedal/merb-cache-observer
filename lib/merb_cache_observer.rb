8bdec3835aedd083f984d10fafd14bfe32cfcb73
Eivind Uggedal <eu@redflavor.com>
2008-07-29 20:18:59 +0200

    Clean up plugin init.

--- lib/merb_cache_observer.rb
+++ lib/merb_cache_observer.rb
@@ -1,12 +1,6 @@
 if defined?(Merb::Plugins)
-
-  require 'merb_cache_observer/cache_observer'
-
   Merb::Plugins.config[:merb_cache_observer] = {}
-  
-  Merb::BootLoader.before_app_loads do
-  end
-  
-  Merb::BootLoader.after_app_loads do
-  end
+
+  require 'dm-observer'
+  require 'merb_cache_observer/cache_observer'
 end
